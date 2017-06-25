b53bcc52ba05af08634a8291e299fef115d6abcf
RandomDentyne <randomdentyne@gmail.com>
2017-06-24 23:32:50 -0700

    fixed js and re-added vendor folder

--- js/slideup.js
+++ js/slideup.js
@@ -1,7 +1,7 @@
 function checkInput() {
     var password = document.getElementById('pass_text').value;
     if(password === "burningsoundfiles") {
-	    document.getElementById('test').slideUp();
+	    $('#test').slideUp();
 	    document.getElementById('header-div').className += " wrapper";
     }
 }
